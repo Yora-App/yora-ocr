95de43f32993c0baac8e2a5145ac556f9b85b278
JulianFP <julian@partanengroup.de>
2026-07-23 14:37:18 +0200

    Add QOS count of 1

--- src/yora_ocr/messaging.py
+++ src/yora_ocr/messaging.py
@@ -13,13 +13,19 @@ def main():
     )
     channel = connection.channel()
 
+    # Set QoS prefetch_count to one, so that rabbitmq only assigns a message to this worker
+    # after it acked the last one
+    channel.basic_qos(prefetch_count=1)
+
     # Ensure that the queue exists
     channel.queue_declare(queue="ocr_jobs_queue", durable=True)
 
     print(" [*] Waiting for messages. To exit press CTRL+C")
     for method, properties, body in channel.consume("ocr_jobs_queue"):
         if (method is not None and properties is not None and body is not None):
+            # placeholder: put ocr here before ack
             print(f" [x] Received {body}")
+
             channel.basic_ack(method.delivery_tag)
 
 
