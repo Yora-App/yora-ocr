c069193a1c8d928d9190e3a78dca6a04572ee80d
Annalena Frey <annalena-frey1@web.de>
2026-07-26 12:44:25 +0200

    first experiments with an llm

--- src/yora_ocr/processing.py
+++ src/yora_ocr/processing.py
@@ -15,7 +15,29 @@ output = llm.create_chat_completion(
             "role": "system",
             "content": "You extract receipt data and return it only in JSON format following the given structure.",
         },
-        {"role": "user", "content": "AldiBanane 1,99Tomate 1,99Apfel 1,50Gesamt 5,48"},
+        {
+            "role": "user",
+            "content": "ALDI SOD"
+            "GalileistraBe 14"
+            "69115 Heidelberg"
+            "EUR"
+            "230334 Champ. WeiB 400g"
+            "1,99A"
+            "231308 Spitzpap. Rot 500g"
+            "0,97A"
+            "229425 Zucchini lose"
+            "0,488 kg x"
+            "1,99 EUR/kg"
+            "K-U-N-D-E-N-B-E-L-E-G"
+            "Kartenzahlung girocard"
+            "Betrag"
+            "33,73 EUR"
+            "05.01.2026"
+            "10:56"
+            "Zahlung erfolgt"
+            "sum"
+            "33,73",
+        },
     ],
     response_format={"type": "json_object", "schema": output_format},
 )
